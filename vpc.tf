dc207c15c9fbff5794cf4f2d6bf65050c293d71c
Ariful Islam <ariful95islam@gmail.com>
2023-10-10 10:58:24 +0100

    edited  cicd

--- vpc.tf
+++ vpc.tf
@@ -2,9 +2,15 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-variable vpc_cidr_block {}
-variable private_subnet_cidr_blocks {}
-variable public_subnet_cidr_blocks {}
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable private_subnet_cidr_blocks {
+    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+variable public_subnet_cidr_blocks {
+    default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
 
 data "aws_availability_zones" "available" {}
 
